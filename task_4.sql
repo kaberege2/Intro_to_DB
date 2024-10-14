e167b62b2d476c5e0b31d4e0c1edab1a906d52a2
kaberege <kabgnestor@gmail.com>
2024-10-14 21:23:52 +0200

    new changes

--- task_4.sql
+++ task_4.sql
@@ -4,12 +4,11 @@ USE alx_book_store;
 -- Query to get full description of the Books table
 SELECT 
     COLUMN_NAME, 
-    DATA_TYPE, 
-    CHARACTER_MAXIMUM_LENGTH AS max_length, 
+    COLUMN_TYPE, 
     IS_NULLABLE, 
     COLUMN_DEFAULT 
 FROM 
-    information_schema.COLUMNS 
+    INFORMATION_SCHEMA.COLUMNS 
 WHERE 
     TABLE_NAME = 'Books' 
     AND TABLE_SCHEMA = 'alx_book_store';
